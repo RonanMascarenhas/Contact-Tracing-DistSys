e6dc5b05d65645b44c617e5ded37798a4c293ecb
HusniKhalaf <44396991+HusniKhalaf@users.noreply.github.com>
2020-12-07 17:20:29 +0000

    Adding subject class to core

---
+++ core/src/main/java/service/core/Subject.java
@@ -0,0 +1,67 @@
+package service.core;
+
+public class Subject {
+    String firstName;
+    String surname;
+    int age;
+    char sex;
+    String phoneNumber;
+    String address;
+
+    public Subject(String firstName, String surname, int age, char sex, String phoneNumber, String address) {
+        this.firstName = firstName;
+        this.surname = surname;
+        this.age = age;
+        this.sex = sex;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
