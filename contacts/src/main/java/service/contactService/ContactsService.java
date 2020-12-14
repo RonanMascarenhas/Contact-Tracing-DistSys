5fbb62479cd23edee63c1723a842d8d39851089e
Finn O'Neill <finn.oneill@ucdconnect.ie>
2020-12-14 16:14:21 +0000

    Rough Rest Database

--- contacts/src/main/java/service/contactService/ContactsService.java
+++ contacts/src/main/java/service/contactService/ContactsService.java
@@ -1,14 +1,18 @@
 package service.contactService;
 
+import com.mongodb.*;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.sql.*;
 
 @RestController
 public class ContactsService {
 
     //Checks patient DB to see if phone number exists in there
     public boolean checkPatientExists(String Phone){
+       /* BasicDBObject searchQuery = new BasicDBObject();
+        searchQuery.put("phonenumber", Phone);
+        DBCursor cursor = patientRepo.find(searchQuery);
+
+       */
         return true;
     }
 
@@ -17,25 +21,4 @@ public class ContactsService {
         return true;
     }
 
-    //Add a contact to the database
-    public void addToContactDB(String Name, String Phone, String Address, String CaseID ){
-
-    }
-
-    public static void main(String[] args){
-
-        String url = "jdbc:mysql://localhost:3306/contactsDB?autoReconnect=true&userSSl=false";
-        String user = "root";
-        String password = "dissys";
-        try {
-            Connection myConn = DriverManager.getConnection(url, user, password);
-            Statement myStmt = myConn.createStatement();
-            String sql = "select * from contactDB";
-            ResultSet resultSet = myStmt.executeQuery(sql);
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-    }
 }
