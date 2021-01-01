7736478c22ccbc2dd1d1e16c88f620458bf837a8
Joseph Dempsey <joseph.dempsey@ucdconnect.ie>
2021-01-01 23:16:04 +0000

    Changed PatientWorkItem to accept patients and generate workItems.
    Signed-off-by: Joe-Demp <dempseyjoseph96@yahoo.com>

--- core/src/main/java/service/messages/PatientWorkItem.java
+++ core/src/main/java/service/messages/PatientWorkItem.java
@@ -13,12 +13,17 @@ public abstract class PatientWorkItem implements Serializable {
     private Status status = Status.TODO;
 
     private String patientId;
-    private String name;
+    private String firstName;
+    private String surname;
     private String phoneNumber;
     private Result result;
 
     public PatientWorkItem(Patient patient) {
-        // todo populate the fields from the patient once the Patient class has been finalized.
+        this.patientId = patient.getId();
+        this.firstName = patient.getFirstName();
+        this.surname = patient.getSurname();
+        this.phoneNumber = patient.getPhoneNumber();
+        this.result = patient.getResult();
     }
 
     public PatientWorkItem() {
@@ -66,12 +71,20 @@ public abstract class PatientWorkItem implements Serializable {
         this.patientId = patientId;
     }
 
-    public String getName() {
-        return name;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
     }
 
     public String getPhoneNumber() {
