c5e17496b400b9087251bb9130d843165d14bb86
Joseph Dempsey <joseph.dempsey@ucdconnect.ie>
2021-01-03 01:25:25 +0000

    Fixed the pom to include the Eureka dependency and the Application to include the EurekaDNS bean properly.
    Signed-off-by: Joe-Demp <dempseyjoseph96@yahoo.com>

--- contact-tracing-scheduler/src/main/java/ie/sds/scheduler/ContactTracingSchedulerApplication.java
+++ contact-tracing-scheduler/src/main/java/ie/sds/scheduler/ContactTracingSchedulerApplication.java
@@ -3,11 +3,10 @@ package ie.sds.scheduler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
-import service.dns.DomainNameService;
 import service.dns.EurekaDNS;
 
 @SpringBootApplication
-@Import({DomainNameService.class, EurekaDNS.class})
+@Import({EurekaDNS.class})
 public class ContactTracingSchedulerApplication {
     public static void main(String[] args) {
         System.exit(SpringApplication.exit(SpringApplication.run(ContactTracingSchedulerApplication.class, args)));
